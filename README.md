fe06fd03d9a9e8cfa391aae08126d80e98b16c95
Jonathan Heard <jon.heard.tutor@gmail.com>
2022-05-27 07:15:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Obsidian Plugin - Text Expander JS
+
+This plugin allows defining text-entry shortcuts to be expanded into strings built using javascript.
+
+## Examples:
+<ul>
+	<li>Random dice roll: `;;d100;` => `75 (D100)`</li>
+	<li>Random name: `;;name male european;` => `Bill Harrington`</li>
+</ul>
+
+## Installation
+TBA
+
+## Settings
+TBA
+
+## Known Issues
+None
+
+## Credits
+This project was inspired by description of Obsidian on the RPG Tips youtube video <a href='https://www.youtube.com/watch?v=XTFFzuZVcPk' target='_blank'>How I play my games in 2021</a>.
+
+This project was made with some reference and allusion to the <a href='https://github.com/konodyuk/obsidian-text-expander' target='_blank'>obsidian-text-expander</a> plugin.
+
+## Release notes
+0.5.0
+<ul>
+	<li>Basic implementation.  All settings hardwired</li>
+</ul>
+
---
+++ main.js
@@ -0,0 +1,172 @@
+'use strict';
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+const SRC_PREFIX = ";;";
+const SRC_SUFFIX = ";";
+const PATTERN_FILE = "TextExpanderJs.json";
+const HOTKEY = "Enter"; // "Enter"
+
+var obsidian = require('obsidian');
+
+var extendStatics = function(d, b) {
+	extendStatics =
+		Object.setPrototypeOf ||
+		(
+			{ __proto__: [] } instanceof Array &&
+			function (d, b) { d.__proto__ = b; }
+		) ||
+		function (d, b)
+		{
+			for (var p in b)
+				if (Object.prototype.hasOwnProperty.call(b, p))
+					d[p] = b[p];
+		};
+	return extendStatics(d, b);
+};
+function __extends(d, b) {
+	extendStatics(d, b);
+	function __() { this.constructor = d; }
+	d.prototype =
+		b === null ?
+		Object.create(b) :
+		(__.prototype = b.prototype, new __());
+}
+
+var MyPlugin = (function (_super)
+{
+	__extends(MyPlugin, _super);
+
+	MyPlugin.prototype.handleHotkey = async function(cm, keydown)
+	{
+		if (event.key === HOTKEY)
+		{
+			let toExpand = this.getToExpand(cm);
+			if (toExpand)
+			{
+				event.preventDefault();
+				await this.runExpander(cm, toExpand);
+			}
+		}
+	};
+
+	MyPlugin.prototype.getToExpand = function(cm)
+	{
+		let cursor = cm.getCursor();
+		let result = { lineIndex: cursor.line, prefixIndex: -1, postfixIndex: -1 };
+		let lineText = cm.getLine(cursor.line);
+		result.prefixIndex = lineText.lastIndexOf(SRC_PREFIX, cursor.ch);
+		result.suffixIndex = lineText.indexOf(SRC_SUFFIX, cursor.ch - SRC_SUFFIX.length);
+		if (result.prefixIndex == -1 || result.suffixIndex == -1) { result = null; }
+		return result;
+	}
+
+	MyPlugin.prototype.runExpander = async function(cm, toExpand)
+	{
+		let patterns = await this.getExpanderPatterns();
+		if (!patterns)
+		{
+			return alert("Invalid expander patterns file\n" + PATTERN_FILE);
+		}
+
+		let text = cm.getLine(toExpand.lineIndex).substring(
+			toExpand.prefixIndex + SRC_PREFIX.length,
+			toExpand.suffixIndex);
+
+		// Find and use the right expander pattern
+		for (let i = 0; i < patterns.length; i++)
+		{
+			let result = text.match(patterns[i].regex);
+
+			if (!result) { continue; }
+			let replacement = "";
+			for (let k = 1; k < result.length; k++)
+			{
+				replacement += "let $" + k + " = " + result[k] + ";\n";
+			}
+			replacement += patterns[i].replacer + ";";
+			replacement = eval(replacement);
+			cm.replaceRange(
+				replacement,
+				{ line: toExpand.lineIndex,
+				  ch: toExpand.prefixIndex },
+				{ line: toExpand.lineIndex,
+				  ch: toExpand.suffixIndex + SRC_SUFFIX.length });
+			break;
+		}
+	};
+
+	MyPlugin.prototype.getExpanderPatterns = async function()
+	{
+		let file = this.app.vault.fileMap["TextExpanderJs.json.md"];
+		if (!this._patternFileModDate || this._patternFileModDate < file.stat.mtime)
+		{
+			let patternText = await this.app.vault.read(file);
+			try
+			{
+				this._patterns = JSON.parse(patternText);
+				this._patternFileModDate = file.stat.mtime;
+			}
+			catch (e)
+			{
+				console.error(e);
+				return null;
+			}
+		}
+		return this._patterns;
+	};
+
+	MyPlugin.prototype.refreshIsHotkeyEnabled = function()
+	{
+		for (let i = 0; i < this._codeMirrors.length; i++)
+		{
+			if (this._isEnabled)
+			{
+				this._codeMirrors[i].on('keydown', this._handleHotkey);
+			}
+			else
+			{
+				this._codeMirrors[i].off('keydown', this._handleHotkey);
+			}
+		}
+	};
+
+	function MyPlugin()
+	{
+		let result = _super !== null && _super.apply(this, arguments) || this;
+
+		this._isEnabled = false;
+
+		// Create version that forces "this" to be "MyPlugin" for file access
+		this._handleHotkey = this.handleHotkey.bind(this);
+
+		this._codeMirrors = this._codeMirrors || [];
+		this.registerCodeMirror((cm) =>
+		{
+			if (!this._codeMirrors.contains(cm))
+			{
+				this._codeMirrors.push(cm);
+				this.refreshIsHotkeyEnabled();
+			}
+		});
+
+		return result;
+	}
+
+	MyPlugin.prototype.onload = function()
+	{
+		this._isEnabled = true;
+		this.refreshIsHotkeyEnabled();
+	};
+
+	MyPlugin.prototype.onunload = function ()
+	{
+		this._isEnabled = false;
+		this.refreshIsHotkeyEnabled();
+ 	};
+
+	return MyPlugin;
+
+}(obsidian.Plugin));
+
+module.exports = MyPlugin;
