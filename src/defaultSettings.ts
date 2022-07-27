11cb5447ee6534e8292934775ee8125ac746bd44
Jon Heard <jon.heard.tutor@gmail.com>
2022-07-27 05:43:49 -0400

    - polish - default shortcuts - updated About strings

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -51,15 +51,8 @@ __
 __
 return "🎲 " + roll($1) + " /D" + $1;
 __
-d{max} - A dice roller shortcut.  Expands into "🎲 {roll result} /D{max}".  {max} is a required parameter: a positive integer giving the size of dice to roll.
+d{max: required, >0} - A dice roller shortcut.  Expands into "🎲 {roll result} /D{max}".  {max} is the size of dice to roll.
     - Examples - d3, d20, d57, d999
-
-__
-^[f|F][d|D]([0-9]+)$
-__
-return "<span style='background-color:lightblue;color:black;padding:0 .25em'>🎲 <b>" + roll($1) + "</b> /D" + $1 + "</span>";
-__
-fd{max} - Same as d{max}, but with fancy formatting.
 
 __
 ^([0-9]*)[d|D]([0-9]+)(|(?:\\+[0-9]+)|(?:\\-[0-9]+))$
@@ -80,7 +73,7 @@ if ($3) {
 if (isNaN(label.substr(1))) { label = "(" + label + ")"; }
 return "🎲 " + result + " /" + label;
 __
-{count}d{max}{add} - Same as d{max}, but with optional {count} and {add} parameters.  {count} is a positive integer giving the number of dice to roll and add together.  {add} is "+" or "-" followed by a positive integer giving the amount to adjust the result by.
+{count: optional, >0}d{max: required, >0}{add: optional, + or - followed by >0} - A dice roller shortcut, same as d{max}, but with optional {count} and {add} parameters.  {count} is the number of dice to roll and add together.  {add} is "+" or "-" followed by an amount to adjust the result by.
     - Examples - d100, 3d20, d10+5, 3d6+6"
 `
 });
